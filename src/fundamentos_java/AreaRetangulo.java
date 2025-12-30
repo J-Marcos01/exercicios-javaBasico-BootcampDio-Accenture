7011b062b4edb2e3c4cea86f2325713911c42c23
Jmarcos <joao.rocha31@fatec.sp.gov.br>
2025-12-29 23:37:16 -0300

    feat: add exercicios 02,03 e 04 primeira lista

---
+++ src/fundamentos_java/AreaRetangulo.java
@@ -0,0 +1,18 @@
+package fundamentos_java;
+
+import java.util.Scanner;
+
+public class AreaRetangulo {
+    public static void main (String[] args) {
+        
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor da base e da altura do retângulo:");
+        var base = scanner.nextDouble();
+        var altura = scanner.nextDouble();
+        var area = base * altura;
+
+        System.out.printf("A área do retângulo é: %.2f %n", area);
+    }
+    
+}
---
+++ src/fundamentos_java/DiferencaIdade.java
@@ -0,0 +1,18 @@
+package fundamentos_java;
+
+import java.util.Scanner;
+
+public class DiferencaIdade {
+    public static void main (String[] args) {
+
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Digite a idade de duas pessoas:");
+        var idadeUsuario1 = scanner.nextInt();
+        var idadeUsuario2 = scanner.nextInt();
+
+        var diferencaIdade = Math.abs(idadeUsuario1 - idadeUsuario2);
+
+        System.out.printf("A diferença de idade entre elas é: %d anos %n", diferencaIdade);
+    }
+}
